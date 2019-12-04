6f43183d6c578a2b317c04746c6888095ee94e7e
parambedi1453 <parambedi1453@gmail.com>
2019-12-04 21:46:21 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express()
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server)
 
-user = []
+users = []
 connections = []
 
 server.listen(process.env.PORT || 3000);
@@ -20,13 +20,28 @@ io.sockets.on('connection',function(socket){
 
     // Disconnect
     socket.on('disconnect' ,function(data){
+        
+        users.splice(users.indexOf(socket.username),1);
+        updateUsernames()
         connections.splice(connections.indexOf(socket),1);
     console.log('Disconnected : %s sockests connected',connections.length);
     })
 
     //send message
     socket.on('send message',function(data){
-        io.sockets.emit('new message',{msg : data});
+        console.log(data);
+        io.sockets.emit('new message',{msg : data ,user:socket.username});
     })
     
+    // new user
+    socket.on('new user' , function(data,callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username)
+        updateUsernames();
+    })
+
+    function updateUsernames(){
+        io.sockets.emit('get users',users)
+    }
 })
